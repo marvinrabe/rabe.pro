505094cdc339cc7d0c7a866a332991d86d746a2b
Marvin Rabe <marvin.rabe@gmail.com>
2021-04-14 23:14:34 +0200

    feat: load font awesome icons as SVG.

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 import Head from 'next/head'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faDev, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home () {
   const socialLinks = [
     {
       title: 'Github',
       url: 'https://github.com/marvinrabe',
-      icon: 'fa-github'
+      icon: faGithub
     },
     {
       title: 'Twitter',
       url: 'https://twitter.com/marvin_rabe',
-      icon: 'fa-twitter'
+      icon: faTwitter
     },
     {
       title: 'DEV',
       url: 'https://dev.to/marvinrabe',
-      icon: 'fa-dev'
+      icon: faDev
     },
     {
       title: 'Stack Overflow',
       url: 'https://stackoverflow.com/users/565323/marvin-rabe',
-      icon: 'fa-stack-overflow'
+      icon: faStackOverflow
     },
   ]
 
@@ -29,27 +31,23 @@ export default function Home () {
       <Head>
         <title>Marvin Rabe | Laravel Developer</title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;700&display=swap" rel="stylesheet" />
-        <link href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;700&display=swap" rel="stylesheet"/>
         <script async defer data-domain="rabe.pro" src="https://stats.rabe.pro/js/index.js"/>
       </Head>
 
-      <div className="text-4xl lg:text-6xl font-bold font-serif text-gray-800 dark:text-gray-100">Hello, I'm Marvin!</div>
-      <div className="text-xl lg:text-2xl font-light font-serif my-8 text-gray-700 dark:text-gray-200">
-        Laravel&nbsp;Developer @&nbsp;<a href="https://nueprice.com/" target="_blank" className='underline hover:no-underline'>nueprice&nbsp;GmbH</a>
+      <div className="text-4xl lg:text-6xl font-bold font-serif text-gray-800 dark:text-gray-100">Hello, I'm Marvin!
       </div>
-      <div className="text-4xl lg:text-6xl text-gray-700 dark:text-gray-200 flex gap-4">
+      <div className="text-xl lg:text-2xl font-light font-serif my-8 text-gray-700 dark:text-gray-200">
+        Laravel&nbsp;Developer @&nbsp;<a href="https://nueprice.com/" target="_blank"
+                                         className='underline hover:no-underline'>nueprice&nbsp;GmbH</a>
+      </div>
+      <div className="text-gray-700 dark:text-gray-200 grid grid-flow-col items-center gap-4">
         {socialLinks.map((item, index) => (
-          <a key={index} href={item.url} title={item.title} target="_blank" className="hover:opacity-75">
-            <span className={`p-2 fab ${item.icon}`}/>
+          <a key={index} href={item.url} title={item.title} target="_blank" className="p-2 hover:opacity-75">
+            <FontAwesomeIcon icon={item.icon} className="w-10 lg:w-14 h-10 lg:h-14"/>
           </a>
         ))}
       </div>
-      <style jsx global>{`
-      body {
-        @apply bg-gray-100;
-      }
-    `}</style>
     </main>
   )
 }
