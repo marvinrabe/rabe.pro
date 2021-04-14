d062b851120625085a49c0dd1a2c0461d7c04abd
Marvin Rabe <marvin.rabe@gmail.com>
2021-04-14 22:42:32 +0200

    feat: add darkmode.

--- pages/index.js
+++ pages/index.js
@@ -25,26 +25,31 @@ export default function Home () {
   ]
 
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
       <Head>
         <title>Marvin Rabe | Laravel Developer</title>
-        <link href="https://fonts.googleapis.com/css?family=Reem+Kufi|Roboto:300" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;700&display=swap" rel="stylesheet" />
         <link href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="css/reset.css"/>
-        <link rel="stylesheet" href="css/styles.css"/>
-        <link rel="stylesheet" href="css/themes/white-grey.css"/>
-        <script async defer data-domain="rabe.pro" src="https://stats.rabe.pro/js/index.js" />
+        <script async defer data-domain="rabe.pro" src="https://stats.rabe.pro/js/index.js"/>
       </Head>
 
-      <div className="intro">Hello, I'm Marvin!</div>
-      <div className="tagline">
-        Laravel Developer @ <a href="https://nueprice.com/" target="_blank" style={{color: 'inherit'}}>nueprice GmbH</a>
+      <div className="text-4xl lg:text-6xl font-bold font-serif text-gray-800 dark:text-gray-100">Hello, I'm Marvin!</div>
+      <div className="text-xl lg:text-2xl font-light font-serif my-8 text-gray-700 dark:text-gray-200">
+        Laravel&nbsp;Developer @&nbsp;<a href="https://nueprice.com/" target="_blank" className='underline hover:no-underline'>nueprice&nbsp;GmbH</a>
       </div>
-      <div className="icons-social">
-        {socialLinks.map(item => (
-          <a target="_blank" href={item.url} title={item.title}><span className={`fab ${item.icon}`}/></a>
+      <div className="text-4xl lg:text-6xl text-gray-700 dark:text-gray-200 flex gap-4">
+        {socialLinks.map((item, index) => (
+          <a key={index} href={item.url} title={item.title} target="_blank">
+            <span className={`p-2 fab ${item.icon}`}/>
+          </a>
         ))}
       </div>
+      <style jsx global>{`
+      body {
+        @apply bg-gray-100;
+      }
+    `}</style>
     </main>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Roboto Slab', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
