9fd8e98ced8bea3f9a24b13fe64140174df38489
Marvin Rabe <marvin.rabe@gmail.com>
2021-04-14 23:16:55 +0200

    feat: add smooth opacity transitions.

--- pages/index.js
+++ pages/index.js
@@ -35,15 +35,16 @@ export default function Home () {
         <script async defer data-domain="rabe.pro" src="https://stats.rabe.pro/js/index.js"/>
       </Head>
 
-      <div className="text-4xl lg:text-6xl font-bold font-serif text-gray-800 dark:text-gray-100">Hello, I'm Marvin!
+      <div className="text-4xl lg:text-6xl font-bold font-serif text-gray-800 dark:text-gray-100">
+        Hello, I'm Marvin!
       </div>
       <div className="text-xl lg:text-2xl font-light font-serif my-8 text-gray-700 dark:text-gray-200">
-        Laravel&nbsp;Developer @&nbsp;<a href="https://nueprice.com/" target="_blank"
-                                         className='underline hover:no-underline'>nueprice&nbsp;GmbH</a>
+        Laravel&nbsp;Developer
+        @&nbsp;<a href="https://nueprice.com/" target="_blank" className='underline hover:no-underline transition-opacity hover:opacity-75'>nueprice&nbsp;GmbH</a>
       </div>
       <div className="text-gray-700 dark:text-gray-200 grid grid-flow-col items-center gap-4">
         {socialLinks.map((item, index) => (
-          <a key={index} href={item.url} title={item.title} target="_blank" className="p-2 hover:opacity-75">
+          <a key={index} href={item.url} title={item.title} target="_blank" className="p-2 transition-opacity hover:opacity-75">
             <FontAwesomeIcon icon={item.icon} className="w-10 lg:w-14 h-10 lg:h-14"/>
           </a>
         ))}
