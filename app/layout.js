7e0386fe09929261eef6bb5a61f6da56c09b49d9
Marvin Rabe <marvin.rabe@gmail.com>
2024-03-18 18:08:18 +0100

    feat: add projects

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout ({ children }) {
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;700&display=swap" rel="stylesheet"/>
       <script defer data-domain="rabe.pro" data-api="/stats/event" src="/stats/script.js"></script>
     </head>
-    <body className={'bg-stone-800'}>
+    <body className={'bg-zinc-800'}>
     {children}
     </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -25,23 +25,59 @@ export default function Home () {
     },
   ]
 
+  let projects = [
+    {
+      title: 'flugschulen.net',
+      url: 'https://flugschulen.net',
+      description: ''
+    },
+    {
+      title: 'fliegerei.blog',
+      url: 'https://fliegerei.blog',
+      description: ''
+    },
+    {
+      title: 'natoalphabet.com',
+      url: 'https://natoalphabet.com',
+      description: ''
+    }
+  ]
+
   return (
-    <main className="flex flex-col items-center justify-center h-full p-8 text-center">
-      <img src="/me.jpg" height="128" width="128" alt="Marvins face" className="border-4 border-stone-800 ring-2 ring-amber-400 rounded-full shadow-sm"/>
-      <h1 className="text-4xl lg:text-6xl font-bold font-serif text-stone-50 mt-12 mb-6">
-        Hello, I'm <span className="text-transparent bg-clip-text bg-gradient-to-br from-amber-400 to-orange-600">Marvin</span>.
+    <main className="flex flex-col items-center justify-center min-h-full p-8 text-center">
+      <img src="/me.jpg" height="128" width="128" alt="Marvins face"
+           className="border-8 border-zinc-800 ring-2 ring-amber-400 rounded-full" />
+      <h1 className="text-4xl lg:text-6xl font-bold font-serif text-zinc-50 mt-12 mb-4">
+        Hello, I'm <span
+        className="text-transparent bg-clip-text bg-gradient-to-br from-amber-400 to-orange-600">Marvin</span>.
       </h1>
-      <p className="text-xl lg:text-2xl font-light mb-12 text-stone-400">
+      <p className="text-xl lg:text-2xl font-light mb-6 text-zinc-400">
         <span className="whitespace-nowrap">Software Developer</span>
       </p>
       <div className="grid grid-flow-col items-center gap-4">
         {socialLinks.map((item, index) => (
           <a key={index} href={item.url} title={item.title} rel="me noreferrer"
-             className="p-2 text-stone-50 transition-color duration-500 hover:text-orange-400">
-            <FontAwesomeIcon icon={item.icon} fixedWidth className="h-8 lg:h-12 w-8 lg:w-12"/>
+             className="p-2 text-zinc-50 transition-color duration-500 hover:text-orange-500">
+            <FontAwesomeIcon icon={item.icon} fixedWidth className="h-6 lg:h-8 w-6 lg:w-8" />
           </a>
         ))}
       </div>
+      <h2 className="text-2xl lg:text-4xl font-bold font-serif text-zinc-50 mt-16 mb-4">My Projects</h2>
+      <div className="bg-gradient-to-br from-amber-400 to-orange-600 rounded-full h-[4px] w-32 mb-8 block">
+        &nbsp;
+      </div>
+      <ul className="mx-auto container flex flex-wrap justify-center gap-6 max-w-[1200px]">
+        {projects.map((item) => (
+          <li>
+            <a href={item.url} target="_blank"
+               className="project flex items-end justify-start h-32 w-[330px] max-w-[330px]">
+            <span className="block text-zinc-50 font-bold tracking-wide text-left p-4">
+              {item.title}
+            </span>
+            </a>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
